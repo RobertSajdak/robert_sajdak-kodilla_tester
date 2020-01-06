5f5cb20ad73c645d3041b3552204b8257d711d85
Robert Sajdak <robert.sajdak@alusalon.com>
2020-01-06 21:45:14 +0100

    rozwiazanie zadania wyscigi samochodowe

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/Opel.java
@@ -0,0 +1,21 @@
+package com.kodilla.collections.interfaces.homework;
+
+public class Opel implements Car {
+
+    int speed = 0;
+
+    @Override
+    public int getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public void increaseSpeed() {
+        this.speed = this.speed + 20;
+    }
+
+    @Override
+    public void decreaseSpeed() {
+        this.speed = this.speed - 10;
+    }
+}
---
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class RandomNumbers {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int a = r.nextInt(31);
+        System.out.println(a);
+    }
+
+
+}
