3e18c051d9925fdc34442e9af260126d10362001
Robert Sajdak <robert.sajdak@alusalon.com>
2020-01-19 16:11:33 +0100

    Zad.niemutowalnosc - po korekcie

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
@@ -1,19 +1,19 @@
 package com.kodilla.collections.adv.immutable.homework;
 
 public final class Task {
-    protected final String title;
-    protected final int duration;
+    private final String title;
+    private final int duration;
 
-    private Task(String title, int duration) {
+    public Task(String title, int duration) {
         this.title = title;
         this.duration = duration;
     }
 
-    private final String getTitle() {
+    public final String getTitle() {
         return title;
     }
 
-    private final int getDuration() {
+    public final int getDuration() {
         return duration;
     }
 }
