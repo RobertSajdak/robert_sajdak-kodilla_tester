dee20713d780f75d69910eb7a0eb742bea8dd18f
Robert Sajdak <robert.sajdak@alusalon.com>
2020-01-20 18:51:14 +0100

    Zadanie księgozbiór

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -2,8 +2,15 @@ package com.kodilla.collections.adv.immutable.special.homework;
 
 public class BookApplication {
     public static void main(String[] args) {
+        BookManager book = new BookManager();
+        Book book1 = book.createBook("Java. Podstawy", "Cay S. Horstmann");
+        Book book2 = book.createBook("Programowanie w języku Java w.2", "Robert Sedgewick, Kevin Wayne");
+        Book book3 = book.createBook("Java. Podstawy", "Cay S. Horstmann");
 
-
-
+        System.out.println(book1);
+        System.out.println(book2);
+        System.out.println(book3);
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("Current collection size is: " + book.size());
     }
 }
--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -1,10 +1,22 @@
 package com.kodilla.collections.adv.immutable.special.homework;
 
+import java.util.HashSet;
+
 public class BookManager {
-
-    public Book createBook (String title, String author) {
-
-
+    public Book createBook (String title, String author){
+        Book book = new Book (title, author);
+        myCollection.add(book);
+        return book;
     }
+
+    private HashSet<Book> myCollection = new HashSet<>();
+    public int size (){
+        return myCollection.size();
+    }
+
+
+
+
+
 
 }
