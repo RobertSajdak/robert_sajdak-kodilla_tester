bf2d2037a2c899b132725660b8321d57a8bddc39
Robert Sajdak <robert.sajdak@alusalon.com>
2020-03-12 14:52:36 +0100

    Zadanie_Wstrzykiwanie beanów

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/TextMessageService.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/TextMessageService.java
@@ -3,7 +3,7 @@ package com.kodilla.spring.basic.dependency_injection;
 public class TextMessageService implements MessageService {
 
     @Override
-    public void send(String message, String receiver) {
+    public String send(String message, String receiver) {
         System.out.println("Sending text [" + message + "] to: " + receiver);
     }
 }
