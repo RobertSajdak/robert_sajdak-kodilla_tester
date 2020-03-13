2f9fd7afdf9719ec93fea43f4d8dfaedc946e1e3
Robert Sajdak <robert.sajdak@alusalon.com>
2020-03-13 19:01:52 +0100

    Zadanie_Uczniowie i nauczyciele

--- kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -2,13 +2,22 @@ package com.kodilla.optional.homework;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Application {
-    public static List<Student> main(String[] args) {
+    public static void main(String[] args) {
         List<Student> students = new ArrayList<>();
-            students.add(new Student("Jan Kowalski", null));
+        students.add(new Student("Robert", new Teacher("Sajdak")));
+        students.add(new Student("Paulina", null));
+        students.add(new Student("Piotr", new Teacher("Paweł")));
+        students.add(new Student("Roksana", null));
 
-
-        return students;
+        for (Student student : students) {
+            Teacher teacher = student.getTeacher();
+            Optional<Teacher> optionalTeacher = Optional.ofNullable(teacher);
+            String teacherName = optionalTeacher.orElse(new Teacher("<undefined>")).getName();
+            String studentName = student.getName();
+            System.out.println("Student: " + studentName + ", teacher: " + teacherName);
+        }
     }
 }
