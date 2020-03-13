d927dfee5d62690cb8ba4cbf521fefe0218ddfd6
Robert Sajdak <robert.sajdak@alusalon.com>
2020-03-13 19:58:14 +0100

    Zadanie_Obsługa błędów

---
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -0,0 +1,29 @@
+package com.kodilla.exception.homework;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Warehouse {
+    private Set<Order> orders;
+
+    public Warehouse() {
+        this.orders = new HashSet<>();
+    }
+
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+
+    public Order getOrder(String number) throws OrderDoesntExistException {
+        return  orders.stream()
+                .filter(o -> o.getNumber().equals(number))
+                .findAny()
+                .orElseThrow(()-> new OrderDoesntExistException());
+    }
+
+    public int getSize(){
+        return orders.size();
+    }
+}
