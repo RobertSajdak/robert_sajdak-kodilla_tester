d503b069c33f3939f842bdd0e57f94d522367e43
Robert Sajdak <robert.sajdak@alusalon.com>
2020-03-13 18:52:17 +0100

    Zadanie_Lista zadań

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskRepository.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskRepository.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TaskRepository {
-    public static List<Task> getTasks() {
-        List<Task> tasks = new ArrayList<>();
-        tasks.add(new Task("Nowak", LocalDate.of(2020,1,15), LocalDate.of(2020,3,22)));
-        tasks.add(new Task("Smith", LocalDate.of(2020,2,22), LocalDate.of(2020,4,5)));
-        tasks.add(new Task("White", LocalDate.of(2020,4,3), LocalDate.of(2020,4,12)));
-        tasks.add(new Task("Black", LocalDate.of(2020,5,6), LocalDate.of(2020,6,8)));
-        tasks.add(new Task("Callan", LocalDate.of(2020,7,30), LocalDate.of(2020,9,4)));
-        return tasks;
+
+    public static List<Task> getTasks(){
+        List<Task> taskRepository = new ArrayList<>();
+        taskRepository.add(new Task("Wystawienie faktur", LocalDate.of(2019, 12, 12), LocalDate.of(2019, 12, 31)));
+        taskRepository.add(new Task("Wysyłka paczek", LocalDate.of(2019, 11 , 1), LocalDate.of(2020, 1, 5)));
+        taskRepository.add(new Task("Rejestracja zamówień", LocalDate.of(2019, 12, 1), LocalDate.of(2019, 12, 15)));
+        taskRepository.add(new Task("Napisanie wiadomości e-mail do klientów", LocalDate.of(2019, 10, 22), LocalDate.of(2019, 10, 28)));
+        return taskRepository;
     }
 }
