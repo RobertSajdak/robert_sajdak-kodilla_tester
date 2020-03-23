50b7b35918096ddf181b41086c7f2373a5f3fd95
Robert Sajdak <robert.sajdak@alusalon.com>
2020-03-23 18:42:23 +0100

    Hibernate_zadania

---
+++ kodilla-cucumber/src/test/java/wallet/WalletSteps.java
@@ -0,0 +1,22 @@
+package wallet;
+
+import io.cucumber.java8.En;
+
+public class WalletSteps implements En {
+
+    public WalletSteps() {
+        Given("I have deposited $200 in my wallet", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java8.PendingException();
+        });
+
+        When("I request $30", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java8.PendingException();
+        });
+        Then("$30 should be dispensed", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new io.cucumber.java8.PendingException();
+        });
+    }
+}
